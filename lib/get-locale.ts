1657d8933ddc1988b965f82281d191c425d95e00
moonlight5782 <53705948+moonlight5782@users.noreply.github.com>
2026-08-13 01:47:53 +0300

    Add server locale resolver

---
+++ lib/get-locale.ts
@@ -0,0 +1,7 @@
+import { cookies } from "next/headers";
+import { normalizeLocale, type Locale } from "./i18n";
+
+export async function getLocale(): Promise<Locale> {
+  const cookieStore = await cookies();
+  return normalizeLocale(cookieStore.get("locale")?.value);
+}
